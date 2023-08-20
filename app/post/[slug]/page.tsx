04123401ba448d595db210bc6e2277d4921ab0eb
limhizy15 <limhizy15@ajou.ac.kr>
2023-08-20 18:34:11 +0900

    fix: MdxRenderer를 분리하여 빌드 오류 수정

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,57 +1,8 @@
-"use client";
-
 import { allPosts } from "@/.contentlayer/generated";
-import { MDXComponents } from "mdx/types";
-import { useMDXComponent } from "next-contentlayer/hooks";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
-
-const mdxComponents: MDXComponents = {
-  h2: (props) => (
-    <h2
-      className="text-xl md:text-2xl mt-12 font-bold text-blue-500"
-      {...props}
-    ></h2>
-  ),
-  h3: (props) => <h3 className="text-xl pt-8 font-bold" {...props}></h3>,
-  ul: (props) => (
-    <ul
-      className="marker:text-blue-500 list-disc pl-5 space-y-3"
-      {...props}
-    ></ul>
-  ),
-  // @ts-ignore
-  code: ({ node, inline, className, children, ...props }) => {
-    const match = /language-(\w+)/.exec(className || "");
-    return !inline && match ? (
-      //@ts-ignore
-      <SyntaxHighlighter
-        language={match[1]}
-        PreTag="div"
-        {...props}
-        style={dark}
-      >
-        {String(children).replace(/\n$/, "")}
-      </SyntaxHighlighter>
-    ) : (
-      <code
-        className="stack bg-base-200 text-black p-2 rounded-lg text-sm"
-        {...props}
-        style={{
-          //@ts-ignore
-          textWrap: "wrap",
-        }}
-      >
-        {children}
-      </code>
-    );
-  },
-};
+import MdxRenderer from "@/components/mdxRenderer";
 
 function getPost(title: string) {
-  const foundPost = allPosts.find(
-    (post) => post.title === decodeURIComponent(title)
-  );
+  const foundPost = allPosts.find((post) => post.title === decodeURI(title));
   return foundPost;
 }
 
@@ -61,7 +12,6 @@ export default function PostDetailPage({
   params: { slug: string };
 }) {
   const data = getPost(params.slug);
-  const MDXComponent = useMDXComponent(data?.body?.code ?? "");
 
   // TODO: 컴포넌트화
   return (
@@ -73,7 +23,7 @@ export default function PostDetailPage({
           </h1>
           <p className="text-gray-500 ">{data?.date}</p>
 
-          <MDXComponent components={mdxComponents} />
+          <MdxRenderer data={data} />
         </div>
       </div>
     </section>
---
+++ components/mdxRenderer.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { MDXComponents } from "mdx/types";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const mdxComponents: MDXComponents = {
+  h2: (props) => (
+    <h2
+      className="text-xl md:text-2xl mt-12 font-bold text-blue-500"
+      {...props}
+    ></h2>
+  ),
+  h3: (props) => <h3 className="text-xl pt-8 font-bold" {...props}></h3>,
+  ul: (props) => (
+    <ul
+      className="marker:text-blue-500 list-disc pl-5 space-y-3"
+      {...props}
+    ></ul>
+  ),
+  // @ts-ignore
+  code: ({ node, inline, className, children, ...props }) => {
+    const match = /language-(\w+)/.exec(className || "");
+    return !inline && match ? (
+      //@ts-ignore
+      <SyntaxHighlighter
+        language={match[1]}
+        PreTag="div"
+        {...props}
+        style={dark}
+      >
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    ) : (
+      <code
+        className="stack bg-base-200 text-black p-2 rounded-lg text-sm"
+        {...props}
+        style={{
+          //@ts-ignore
+          textWrap: "wrap",
+        }}
+      >
+        {children}
+      </code>
+    );
+  },
+};
+
+export default function MdxRenderer({ data }: { data: any }) {
+  const MDXComponent = useMDXComponent(data?.body?.code ?? "");
+
+  return <MDXComponent components={mdxComponents} />;
+}
