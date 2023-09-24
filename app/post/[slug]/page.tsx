a873bdb57b66a150b9114fa1967d076ac83283f1
limhizy15 <limhizy15@ajou.ac.kr>
2023-09-24 10:14:03 +0900

    feat: og metadata 추가

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { allPosts } from "@/.contentlayer/generated";
 import MdxRenderer from "@/app/_components/NdxRenderer";
 import { Container, Divider, Heading, Text } from "@chakra-ui/react";
+import Head from "next/head";
 
 function getPost(title: string) {
   const foundPost = allPosts.find(
@@ -19,6 +20,18 @@ export default function PostDetailPage({
   // TODO: 컴포넌트화
   return (
     <Container maxW={"container.md"} paddingBlock={8}>
+      <Head>
+        <title>{data?.title} | HEEJI.DEV</title>
+        <meta
+          property="og:url"
+          content={`https://heeji.dev/posts/${data?.title}`}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={`${data?.title} | HEEJI.DEV`} />
+        <meta property="og:title" content={`${data?.title} | HEEJI.DEV`} />
+        <meta property="og:description" content={data?.description} />
+        <meta property="og:image" content={data?.thumbnailURL} />
+      </Head>
       <Heading>{data?.title}</Heading>
 
       <Text color={"gray.400"}>{data?.date}</Text>
